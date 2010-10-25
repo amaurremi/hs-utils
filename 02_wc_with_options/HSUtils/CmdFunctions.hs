2d488298cf4bd482b6fcab0190587b09035efb30
amaurremi <amaurremi@ya.ru>
2010-10-25 19:57:01 +0400

    CmdFunctions.hs restored

---
+++ 02_wc_with_options/HSUtils/CmdFunctions.hs
@@ -0,0 +1,28 @@
+module HSUtils.CmdFunctions where
+
+import System.IO
+import System.Environment
+import System.Exit
+
+readHandleWithPath :: String -> IO (String, Handle)
+readHandleWithPath []  = error "No arguments." >> exitFailure 
+readHandleWithPath "-" = return ("stdin", stdin)
+readHandleWithPath f   = do
+                            file <- openFile f ReadMode
+                            return (f, file)
+
+getReadHandles :: IO [(String, Handle)]
+getReadHandles = do
+                    args <- getArgs
+                    mapM readHandleWithPath args
+
+execute :: ((String, Handle) -> IO a) -> IO [a]
+execute f = do
+               handles <- getReadHandles
+               mapM f handles
+
+execute_ :: ((String, Handle) -> IO a) -> IO ()
+execute_ f = do
+               handles <- getReadHandles
+               mapM_ f handles
+
